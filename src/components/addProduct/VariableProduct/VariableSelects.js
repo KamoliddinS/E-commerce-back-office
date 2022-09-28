54f15c0450329dec25c699878e0b30c07e71d74e
UlugbekNG <andynestrov@yandex.com>
2022-09-28 19:09:31 +0500

    edited variable product add

--- src/components/addProduct/VariableProduct/VariableSelects.js
+++ src/components/addProduct/VariableProduct/VariableSelects.js
@@ -6,21 +6,40 @@ import {
   Box,
   Stack,
   Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Checkbox,
+  ListItemText,
+  OutlinedInput,
 } from "@mui/material";
 import Iconify from "../../Iconify";
-
-import { useSelector } from "react-redux";
+import { useFormik } from "formik";
+import { useSelector, useDispatch } from "react-redux";
+import { addVariableProduct } from "../../../redux/slices/addProductSlice";
 
 const colors = [
   { name: "Black", hex: "#000000" },
   { name: "White", hex: "#ffffff" },
   { name: "Red", hex: "#d51919" },
   { name: "Green", hex: "#00AB55" },
-  { name: "Bleu", hex: "#076be7" },
+  { name: "Blue", hex: "#076be7" },
   { name: "Yellow", hex: "#ffc800e3" },
 ];
 
 export default function VariableSelects() {
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      colors: [],
+    },
+    // validationSchema: validationSchema,
+    onSubmit: (values) => {
+      dispatch(addVariableProduct(values));
+    },
+  });
   const category = useSelector(
     (state) => state.addProduct.baseProduct.category
   );
@@ -50,13 +69,17 @@ export default function VariableSelects() {
         </Breadcrumbs>
       </Typography>
       <Divider />
-      <Stack direction="row" spacing={2} sx={{ marginTop: 5 }}>
-        <Typography variant="body1" gutterBottom>
-          Mavjud ranglar
-        </Typography>
+      <Stack
+        direction="row"
+        alignItems="center"
+        spacing={2}
+        sx={{ marginTop: 5 }}
+      >
+        {console.log(formik.values.colors)}
+        <Typography variant="body1">Mavjud ranglar</Typography>
         <Box sx={{ display: "flex" }}>
           <Stack direction="row" spacing={0.5}>
-            {colors.map((color) => (
+            {formik.values.colors.map((color) => (
               <Box
                 sx={{
                   width: 20,
@@ -66,9 +89,9 @@ export default function VariableSelects() {
                   position: "relative",
                   alignItems: "center",
                   justifyContent: "center",
-                  bgcolor: color.hex,
-                  boxShadow: `0 2px 15px 0 ${color.hex}`,
+                  bgcolor: color,
                   border: "1px solid #e0e0e0",
+                  boxShadow: "0 8px 16px 0 rgb(0 0 0 / 15%)",
                   transition: (theme) =>
                     theme.transitions.create("all", {
                       duration: theme.transitions.duration.shortest,
@@ -77,13 +100,64 @@ export default function VariableSelects() {
               ></Box>
             ))}
           </Stack>
+        </Box>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl sx={{ m: 1, width: 300 }}>
+            <InputLabel id="demo-multiple-checkbox-label">Ranglar</InputLabel>
+            <Select
+              labelId="demo-multiple-checkbox-label"
+              id="demo-multiple-checkbox"
+              multiple
+              name="colors"
+              value={formik.values.colors}
+              onChange={formik.handleChange}
+              input={<OutlinedInput label="Ranglar" />}
+              renderValue={(selected) => selected.join(", ")}
+            >
+              {colors.map((color) => (
+                <MenuItem key={color.name} value={color.hex}>
+                  <Checkbox
+                    checked={formik.values.colors.indexOf(color.hex) > -1}
+                  />
+                  <ListItemText
+                    primary={
+                      <Box sx={{ display: "flex" }}>
+                        <Box
+                          sx={{
+                            width: 20,
+                            height: 20,
+                            display: "flex",
+                            borderRadius: "50%",
+                            position: "relative",
+                            marginRight: 1,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            bgcolor: color.hex,
+                            border: "1px solid #e0e0e0",
+                            boxShadow: "0 8px 16px 0 rgb(0 0 0 / 15%)",
+                            transition: (theme) =>
+                              theme.transitions.create("all", {
+                                duration: theme.transitions.duration.shortest,
+                              }),
+                          }}
+                        ></Box>
+                        <Typography>{color.name}</Typography>
+                      </Box>
+                    }
+                  />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
           <Button
             variant="outlined"
+            type="submit"
             startIcon={<Iconify icon="akar-icons:circle-plus-fill" />}
           >
-            Delete
+            Rang Qo'shish
           </Button>
-        </Box>
+        </form>
       </Stack>
     </>
   );
